fb2ad0c04cbebc13055fd7de57f64a64b70b8f17
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2023-03-27 19:01:58 +0700

    Inject libBedrock

--- src/NhanAZ/BetterCancel/ForceFieldParticle.php
+++ src/NhanAZ/BetterCancel/ForceFieldParticle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace NhanAZ\BetterCancel;
 
+use NhanAZ\libBedrock\BedrockMath;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
 use pocketmine\network\mcpe\protocol\types\LevelEvent;
@@ -12,10 +13,12 @@ use pocketmine\player\Player;
 class ForceFieldParticle {
 
 	public static function addParticle(Vector3 $vector3, Player $player): void {
-		$packet = new LevelEventPacket();
-		$packet->eventId = LevelEvent::PARTICLE_BLOCK_FORCE_FIELD;
-		$packet->eventData = $eventData = (int) 0;
-		$packet->position = Math::Center($vector3);
-		$player->getNetworkSession()->sendDataPacket($packet);
+		$player->getNetworkSession()->sendDataPacket(
+			LevelEventPacket::create(
+				eventId: LevelEvent::PARTICLE_BLOCK_FORCE_FIELD,
+				eventData: 0,
+				position: BedrockMath::Center($vector3)
+			)
+		);
 	}
 }
