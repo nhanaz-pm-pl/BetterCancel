d18e51decb94375135c6e2cb29b71c8576920118
skymim <ppt00003333@gmail.com>
2023-04-02 23:41:35 +0900

    Remove duplicate code & slight improvement

--- src/NhanAZ/BetterCancel/ForceFieldParticle.php
+++ src/NhanAZ/BetterCancel/ForceFieldParticle.php
@@ -8,17 +8,14 @@ use NhanAZ\libBedrock\BedrockMath;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
 use pocketmine\network\mcpe\protocol\types\LevelEvent;
-use pocketmine\player\Player;
 
-class ForceFieldParticle {
+final class ForceFieldParticle {
 
-	public static function addParticle(Vector3 $vector3, Player $player): void {
-		$player->getNetworkSession()->sendDataPacket(
-			LevelEventPacket::create(
-				eventId: LevelEvent::PARTICLE_BLOCK_FORCE_FIELD,
-				eventData: 0,
-				position: BedrockMath::Center($vector3)
-			)
+	public static function getPacket(Vector3 $vector3): LevelEventPacket {
+		return LevelEventPacket::create(
+			eventId: LevelEvent::PARTICLE_BLOCK_FORCE_FIELD,
+			eventData: 0,
+			position: BedrockMath::Center($vector3)
 		);
 	}
 }
